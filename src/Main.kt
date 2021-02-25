6ef1db2673c9a52a3baa0ec897c96c1ad4fdea11
Dmitry Pevnitsky <pandarec13@yandex.ru>
2021-02-25 14:01:52 +0300

    let with

--- src/Main.kt
+++ src/Main.kt
@@ -1,58 +1,16 @@
+import kotlin.random.Random
 
-
+val list: MutableList<Int>? = mutableListOf()
 fun main() {
-    val data = mapOf(
-            "Январь" to listOf(100,100,100,100), // 400
-            "Февраль" to listOf(200,200,-190,200),
-            "Март" to listOf(300,180,300,100), // 880
-            "Апрель" to listOf(250,-250,100,300),
-            "Май" to listOf(200,100,400,300), //1000
-            "Июнь" to listOf(200,100,300,300), // 900
-            )
 
-    printInfo(data)
-
-
+list?.let {
+    with(it) {
+        for (i in 0 until 1000)
+            add(Random.nextInt(0, 1000))
+        val result = it.filter { it%2==0 }.take(100)
+        result.forEach { println(it) }
+    }
 }
-fun printInfo(data: Map<String,List<Int>>):Unit{
-    // средняя в неделю
-    // средняя в месяц
-    // макс в месяц + название
-    // мин в месяц + название
-    // месяцы в которых ошибка
-
-    val mapWithErrors = data.filter { it.value.any { it<0 } }
-    val mapWithoutErrors = data.filter { it.value.all { it>=0 } }
-
-    val averageInWeek = mapWithoutErrors.flatMap { it.value }.average()
-    val averageInMonth = mapWithoutErrors.flatMap { it.value }.sum().toDouble()/mapWithoutErrors.size
-
-    val maxInMonthValues = mapWithoutErrors.map { it.value.sum() }.max()
-    val minInMonthValues = mapWithoutErrors.map { it.value.sum() }.min()
-
-
-    val maxInMonthsNames = mapWithoutErrors.filter { it.value.sum()==maxInMonthValues }
-    val minInMonthsNames = mapWithoutErrors.filter { it.value.sum()==minInMonthValues }
-
-    println(averageInWeek)
-    println(averageInMonth)
-    println(maxInMonthValues)
-    for(month in maxInMonthsNames){
-        println(month.key)
-    }
-    println(minInMonthValues)
-    for(month in minInMonthsNames){
-        println(month.key)
-    }
-
-    for(month in mapWithErrors){
-        println(month.key)
-    }
-
-
-
-
-
 
 
 }
