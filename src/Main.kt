0065135901c514633409c7d2ec99cc7b3b053f79
Dmitry Pevnitsky <pandarec13@yandex.ru>
2021-02-25 14:43:38 +0300

    my with (extension fun)

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,15 @@
-import kotlin.math.sqrt
 
 fun main() {
 
-    val a = 1
 
-    val b = 22
-
-    println(a.isSimple())
-    println(b.isSimple())
 
 }
 
-fun Int.isSimple(): Boolean {
-    if(this < 2)
-        return true
-    for (i in 2 until sqrt(this.toDouble()).toInt()){
-        if(this%i==0)
-            return false
-    }
-    return true
+inline fun <T,R> myWith(obj:T, operation:T.()->R):R{
+    return obj.operation()
 }
+
+
 
 
 
