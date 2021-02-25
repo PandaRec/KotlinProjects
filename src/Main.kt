3ca3733ba8dec44bc4cfd3b555490e517a2dfc74
Dmitry Pevnitsky <pandarec13@yandex.ru>
2021-02-25 14:15:02 +0300

    inline fun with fun operator

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,16 @@
 import kotlin.random.Random
 
-val list: MutableList<Int>? = mutableListOf()
 fun main() {
+    val list= List(100){Random.nextInt(0,100)}
 
-list?.let {
-    with(it) {
-        for (i in 0 until 1000)
-            add(Random.nextInt(0, 1000))
-        val result = it.filter { it%2==0 }.take(100)
-        result.forEach { println(it) }
-    }
+    println(modifyList(list) { it.sum() })
+
+
+
 }
 
-
+inline fun modifyList (list: List<Int>,modify:(List<Int>)->Int): Int{
+    return modify(list)
 }
 
 
