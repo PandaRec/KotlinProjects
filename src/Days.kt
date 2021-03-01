1e944557634656eaf1a18f8bc9b9a583f8469285
Dmitry Pevnitsky <pandarec13@yandex.ru>
2021-03-01 21:44:19 +0300

    companion object in kotlin = static in java with enum

---
+++ src/Days.kt
@@ -0,0 +1,6 @@
+enum class Days(val russiaTr:String) {
+    MONDAY("Понедельник"),TUESDAY("Вторник"),
+    WEDNESDAY("Среда"),THURSDAY("Четверг"),
+    FRIDAY("Пятница"),SATURDAY("Сббота"),
+    SUNDAY("Воскресенье")
+}
--- src/MyRandom.kt
+++ src/MyRandom.kt
@@ -2,34 +2,27 @@ import kotlin.random.Random
 
 class MyRandom {
     companion object{
-        private const val MONDAY = "Monday"
-        private const val TUESDAY = "Tuesday"
-        private const val WEDNESDAY = "Wednesday"
-        private const val THURSDAY = "Thursday"
-        private const val FRIDAY = "Friday"
-        private const val SATURDAY = "Saturday"
-        private const val SUNDAY = "Sunday"
+//        private const val MONDAY = "Monday"
+//        private const val TUESDAY = "Tuesday"
+//        private const val WEDNESDAY = "Wednesday"
+//        private const val THURSDAY = "Thursday"
+//        private const val FRIDAY = "Friday"
+//        private const val SATURDAY = "Saturday"
+//        private const val SUNDAY = "Sunday"
         fun random(startNum:Int, endNum:Int)=
             Random.nextInt(startNum,endNum)
 
 
         fun random() = Random.nextBoolean()
 
-        fun randomDay():String {
-            val listOfDays = mutableListOf<String>()
-            listOfDays.add(MONDAY)
-            listOfDays.add(TUESDAY)
-            listOfDays.add(WEDNESDAY)
-            listOfDays.add(THURSDAY)
-            listOfDays.add(FRIDAY)
-            listOfDays.add(SATURDAY)
-            listOfDays.add(SUNDAY)
-
-            return listOfDays.shuffled().first()
+        fun randomDay()  =
+             Days.values().toList().shuffled().first().russiaTr
 
 
 
 
-        }
+
+
+
     }
 }
