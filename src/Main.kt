bbbcc4305c29f50b313d3aba61c055269ff90cfc
Dmitry Pevnitsky <pandarec13@yandex.ru>
2021-03-01 19:53:33 +0300

    delete anon class and some refactoring

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,14 @@
 fun main() {
 
     val sportsmen = Sportsmen()
-    sportsmen.getVodonos(object : Vodonos{
-        override fun bringWater() {
-            println("bringing water...")
+//    sportsmen.getVodonos(object : Vodonos{
+//        override fun bringWater() {
+//            println("bringing water...")
+//
+//        }
+//    })
 
-        }
-    })
+    sportsmen.getVodonos { println("bringing water...") }
 
 
 }
--- src/Sportsmen.kt
+++ src/Sportsmen.kt
@@ -2,4 +2,7 @@ class Sportsmen {
     fun getVodonos(vodonos: Vodonos){
         vodonos.bringWater()
     }
+    inline fun getVodonos(bringWater: ()-> Unit){
+        bringWater()
+    }
 }
